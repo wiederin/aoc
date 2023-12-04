fee469dc8f13b3dda758711f48e3d2b88f442dcb
wiederin <wiederinchristoph@gmail.com>
2023-12-04 17:08:19 +0100

    2023 - go - d4

--- 2023/go/d4/main.go
+++ 2023/go/d4/main.go
@@ -9,6 +9,7 @@ import (
 )
 
 type Data struct {
+  Winners []int
   Lines []string
 }
 
@@ -23,13 +24,83 @@ func parse(fileName string) (*Data, error) {
 
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
+    var myCardWinningNumbers []string
 		line := scanner.Text()
     if line == "" {
       continue
     }
     d.Lines = append(d.Lines, line)
+    parts := strings.Split(line, ":")
+    if len(parts) != 2 {
+      log.Fatal("Invalid input")
+    }
+    cardNumbers := parts[1]
+    numberParts := strings.Split(cardNumbers, "|")
+    if len(numberParts) != 2 {
+      log.Fatal("Invalid input")
+    }
+    winningNumbers := strings.TrimSpace(numberParts[0])
+    myNumbers := strings.TrimSpace(numberParts[1])
+    winningNumbersList := strings.Split(winningNumbers, " ")
+    myNumbersList := strings.Split(myNumbers, " ")
+    // for each number check if in winningNumbers
+    for _, num := range myNumbersList {
+      if num == "" {
+        continue
+      }
+      for _, wNum := range winningNumbersList {
+        if wNum == num {
+          myCardWinningNumbers = append(myCardWinningNumbers, num)
+        }
+      }
+    }
+    d.Winners = append(d.Winners, len(myCardWinningNumbers))
   }
   return d, nil
+}
+
+func (d *Data)copyCards(i int, wins int, copies *map[int][]int, numCards *int) {
+  *numCards += wins
+  for j := 1; j <= wins; j++ {
+    if i + j > len(d.Winners) {
+      continue
+    }
+    (*copies)[i+j] = append((*copies)[i+j], d.Winners[i+j])
+  }
+}
+
+func part2() {
+  d, err := parse("input.prod")
+  if err != nil {
+    fmt.Println("error parsing")
+  }
+
+  points := 0
+  copies := map[int][]int{}
+  numCards := len(d.Lines)
+
+  for i, wins := range d.Winners {
+    if wins > 0 {
+      cardPoints := 1
+      for j := 1; j < wins; j++ {
+          cardPoints *= 2
+      }
+      points += cardPoints
+    }
+    d.copyCards(i, wins, &copies, &numCards)
+
+    for n, copy := range copies {
+      if n > i {
+        continue
+      }
+      for _, copyInst := range copy {
+        d.copyCards(n, copyInst, &copies, &numCards)
+      }
+      delete(copies, n)
+    }
+  }
+  fmt.Printf("Part 1: %d\n", points)
+  fmt.Printf("Part 2: %d\n", numCards)
 }
 
 func part1() {
@@ -80,5 +151,5 @@ func part1() {
 }
 
 func main() {
-  part1()
+  part2()
 }
