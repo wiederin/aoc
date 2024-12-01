6a270ae7204e20cf1b06b3c9e21e69139e987342
wiederin <wiederinchristoph@gmail.com>
2024-12-02 00:33:54 +0100

    2024 - d1

---
+++ 2024/d1/go.mod
@@ -0,0 +1,3 @@
+module github.com/wiederin/aoc/2024/d1
+
+go 1.23.3
---
+++ 2024/d1/main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"log"
+	"strconv"
+	"strings"
+)
+
+type Node struct {
+	data int
+	next *Node
+}
+
+type LinkedList struct {
+	head *Node
+}
+
+func (loc *Node) insertNode(node Node) {
+	if loc.next == nil {
+		loc.next = &node
+	} else {
+		if loc.next.data > node.data {
+			node.next = loc.next
+			loc.next = &node
+		} else {
+			loc.next.insertNode(node)
+		}
+	}
+}
+
+func (linkedList *LinkedList) traverseList() {
+	if linkedList.head != nil {
+		fmt.Println(linkedList.head.data)
+		if linkedList.head.next != nil {
+			linkedList.head.next.printRecursive()
+		}
+	}
+	
+}
+
+func (node *Node) printRecursive() {
+	fmt.Println(node.data)
+	if node.next != nil {
+		node.next.printRecursive()
+	}
+}
+
+
+func sumDistances(leftNode *Node, rightNode *Node, sum *int) {
+	var dist int
+	if leftNode.data > rightNode.data {
+		dist = leftNode.data - rightNode.data 
+	} else {
+		dist = rightNode.data - leftNode.data
+	}
+	
+	*sum += dist
+	if leftNode.next != nil {
+		sumDistances(leftNode.next, rightNode.next, sum)
+	}
+}
+
+func findNumberOfAppearances(data int, loc *Node, result *int)  {
+	if data == loc.data {
+		*result += 1	
+		if loc.next != nil {
+			findNumberOfAppearances(data, loc.next, result)
+		}
+	} else {
+		if data > loc.data {
+			if loc.next != nil {
+				findNumberOfAppearances(data, loc.next, result)
+			}
+		}
+	}
+}
+
+func calculateSimilarityScore(leftNode *Node, rightNode *Node, score *int) {
+	// find number of appearances
+	appearances := 0
+	findNumberOfAppearances(leftNode.data, rightNode, &appearances) 
+	fmt.Println(appearances)
+
+	// multiply be number
+	nodeScore := appearances * leftNode.data
+	// add to score
+	*score += nodeScore
+	if leftNode.next != nil {
+		calculateSimilarityScore(leftNode.next, rightNode, score)
+	}
+}
+func main() {
+	f, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	var leftList, rightList LinkedList
+	first := true
+	for scanner.Scan() {
+		digits := strings.Split(scanner.Text(), "   ")	
+		if len(digits) == 2 {
+			leftNum, err := strconv.Atoi(digits[0])
+			rightNum, err := strconv.Atoi(digits[1])
+			leftNode := Node{data : leftNum} 
+			rightNode := Node{data : rightNum} 
+			if err != nil {
+				fmt.Println("failed to parse number")
+			}
+			if first {
+				leftList.head = &leftNode
+				rightList.head = &rightNode
+				first = false
+			} else {
+				if leftList.head.data > leftNum {
+					leftNode.next = leftList.head
+					leftList.head = &leftNode 
+				} else {
+					leftList.head.insertNode(leftNode)
+				}
+				if rightList.head.data > rightNum {
+					rightNode.next = rightList.head
+					rightList.head = &rightNode 
+				} else {
+					rightList.head.insertNode(rightNode)
+				}
+				
+			}
+		}
+	}
+
+	sum := 0
+	sumDistances(leftList.head, rightList.head, &sum)
+
+
+	score := 0
+	calculateSimilarityScore(leftList.head, rightList.head, &score)
+
+	fmt.Printf("sum: %d\n", sum)
+	fmt.Printf("score: %d\n", score)
+
+
+}
